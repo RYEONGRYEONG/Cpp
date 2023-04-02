796a3b27fad47fdb0d7a5c2a9fd98303ffddd338
Jo, Seongryeong <126739223+RYEONGRYEONG@users.noreply.github.com>
2023-04-02 20:03:32 +0900

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 int main() {
 	string data;
-	int count = 1;
+	int count = 1; // count 1부터 세기
 	getline(cin, data);
 
 	int len = data.length();
 
 	for (int i = 0; i < data.length(); i++) {
-		if (data[i] == ' ')
+		if (data[i] == ' ') // 공백 있을 때마다 count++
 			count++;
 	}
 
